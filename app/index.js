3f871e6bf424f264a6b0acb0dfa234b4d9c2d3d6
NathanScott101 <nathanscott101@gmail.com>
2021-06-14 20:19:20 +0100

    Working Newer Versions

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import {zeroPad, addDecimalPoint} from "../common/utils";
+import { zeroPad, addDecimalPoint } from "../common/utils";
 import { loadSettings, settings } from "./settingsUtil";
 import document from "document";
 import clock from "clock";
@@ -13,58 +13,200 @@ const setText = document.getElementById("set-text");
 const setTextShadow = document.getElementById("set-text-shadow");
 const currentTime = document.getElementById("time-text");
 const currentTimeShadow = document.getElementById("time-text-shadow");
+const currentSetMode = document.getElementById("current-set-mode");
 const circle = document.getElementById("circ");
-const startButton = document.getElementById("button-start");
 
-const btnPlay = document.getElementById("btn-play");
-const btnPause = document.getElementById("btn-pause");
-const btnSkip = document.getElementById("btn-skip");
-const btnReset = document.getElementById("btn-reset");
+const startButton = document.getElementById("button-start");
+const resetButton = document.getElementById("button-reset");
+const continuePauseButton = document.getElementById("button-pause");
 
 countdownText.text = countdownShadow.text = "Ready";
+currentSetMode.style.display = 'none'
+setText.style.display = setTextShadow.style.display = 'none'
 clock.granularity = "minutes";
 display.autoOff = false;
 
-let interval = settings.timerValue;
-let currentTimer = interval;
+let restTimeInterval = settings.restTimerValue;
+let currentTimer = restTimeInterval;
+let exerciseTimeInterval = settings.exerciseTimerValue;
 let lastValueTimestamp = Date.now();
 let timeElapsed;
 let countdownInterval;
 let setCount = 1;
 let activeFlag = false;
+let nextTimerRest = true;
+
 loadSettings();
-
-
+setVariables();
+resetTimer();
+clearInterval(countdownInterval);
+resetFunc();
 startButton.text = "Start";
 
-function timerCountdown() {
-    timeElapsed = (Date.now() - lastValueTimestamp) / 1000;
-    let updated = Math.round((currentTimer - timeElapsed) * 10);
-    let string = updated.toString();
-    if (updated <= 0) {
-        vibration.start("nudge-max");
-        resetTimer(true);
-    }
-    countdownText.text = countdownShadow.text = addDecimalPoint(string);
-    circle.sweepAngle = Math.round(countdownText.text / interval * 100 * 1.8);
+
+function keepAwake() {
     display.poke();
 }
 
+function timerCountdown() {
+
+    timeElapsed = (Date.now() - lastValueTimestamp) / 1000;
+
+    let updated = Math.round((currentTimer - timeElapsed) * 10);
+    let string = updated.toString();
+    countdownText.text = countdownShadow.text = addDecimalPoint(updated.toString());
+
+    if (settings.exerciseToggle && nextTimerRest) {
+        circle.sweepAngle = Math.round(countdownText.text / settings.exerciseTimerValue * 100 * 1.8);
+    } else {
+        circle.sweepAngle = Math.round(countdownText.text / settings.restTimerValue * 100 * 1.8);
+    }
+    if (Date.now() <= lastValueTimestamp || !(parseFloat(string) > 0)) {
+        vibration.start("ping")
+        resetTimer();
+    }
+    display.poke();
+}
+
+function setVariables() {
+    clearInterval(countdownInterval);
+    startButton.style.display = 'inline'
+    activeFlag = false;
+    setCount = 0;
+    restTimeInterval = settings.restTimerValue;
+    exerciseTimeInterval = settings.exerciseTimerValue;
+}
+
+function resetTimer() {
+    clearInterval(countdownInterval);
+    if (settings.exerciseToggle) {
+        if (nextTimerRest) {
+            setText.style.display = setTextShadow.style.display = 'inline'
+            setCount = setCount + 1;
+            nextTimerRest = false;
+            currentSetMode.text = 'Rest';
+            currentTimer = settings.restTimerValue;
+            activeFlag = true;
+            lastValueTimestamp = Date.now();
+            countdownInterval = setInterval(timerCountdown, 100);
+            setPlaying();
+
+
+        } else {
+            nextTimerRest = true;
+            currentSetMode.text = 'Exercise'
+            currentTimer = exerciseTimeInterval;
+            activeFlag = true;
+            lastValueTimestamp = Date.now();
+            countdownInterval = setInterval(timerCountdown, 100);
+            setPlaying();
+        }
+    } else {
+
+        activeFlag = false;
+        countdownText.text = countdownShadow.text = `Ready`;
+        currentSetMode.text = 'Exercise'
+        continuePauseButton.text = 'Continue'
+    }
+    setText.text = setTextShadow.text = `Set ` + setCount;
+}
+
 startButton.addEventListener("click", (evt) => {
-    console.log("CLICKED");
-    activeFlag = true;
-    lastValueTimestamp = Date.now();
-    countdownInterval = setInterval(timerCountdown, 100);
-    setPlaying();
-  })
+    vibration.start("confirmation-max")
+    if (settings.exerciseToggle) {
+        restTimeInterval = settings.restTimerValue;
+        currentSetMode.text = 'Exercise'
+        currentSetMode.style.display = 'inline'
+        currentTimer = settings.exerciseTimerValue;
+        nextTimerRest = true;
+        activeFlag = true;
+        lastValueTimestamp = Date.now();
+        countdownInterval = setInterval(timerCountdown, 100);
+        setPlaying();
+
+    } else {
+        setText.style.display = setTextShadow.style.display = 'inline'
+        currentSetMode.style.display = 'inline'
+        setCount = setCount + 1;
+        setText.text = setTextShadow.text = `Set ` + setCount;
+        currentSetMode.text = 'Rest'
+        restTimeInterval = settings.restTimerValue;
+        currentTimer = restTimeInterval;
+        activeFlag = true;
+        lastValueTimestamp = Date.now();
+        countdownInterval = setInterval(timerCountdown, 100);
+        setPlaying();
+    }
+})
+
+continuePauseButton.addEventListener("click", (evt) => {
+    vibration.start("confirmation-max")
+    if (parseFloat(countdownText.text) > 1 || countdownText.text === 'Ready') {
+        if (settings.exerciseToggle) {
+            if (activeFlag == true) {
+                activeFlag = false;
+                currentTimer = countdownText.text;
+                clearInterval(countdownInterval);
+                continuePauseButton.text = 'Continue'
+            } else {
+                activeFlag = true;
+                lastValueTimestamp = Date.now();
+                countdownInterval = setInterval(timerCountdown, 100);
+                continuePauseButton.text = 'Pause'
+            }
+        }
+        else {
+            if (activeFlag == true) {
+                activeFlag = false;
+                currentTimer = countdownText.text;
+                clearInterval(countdownInterval);
+                continuePauseButton.text = 'Continue'
+            } else {
+                if (countdownText.text === 'Ready') {
+                    setCount = setCount + 1;
+                    setText.text = setTextShadow.text = `Set ` + setCount;
+                    currentTimer = restTimeInterval;
+                    currentSetMode.text = 'Rest'
+                }
+                activeFlag = true;
+                lastValueTimestamp = Date.now();
+                countdownInterval = setInterval(timerCountdown, 100);
+                continuePauseButton.text = 'Pause'
+
+            }
+        }
+    }
+}
+)
+
+resetButton.addEventListener("click", (evt) => {
+    resetFunc();
+})
+
+function resetFunc() {
+    startButton.style.display = 'inline'
+    currentSetMode.style.display = 'none'
+    setText.style.display = setTextShadow.style.display = 'none'
+    continuePauseButton.text = 'Pause'
+    countdownText.text = countdownShadow.text = `Ready`;
+    setText.text = setTextShadow.text = `Set ` + setCount;
+    setCount = 0
+    activeFlag = false;
+    nextTimerRest = false;
+    clearInterval(countdownInterval);
+    restTimeInterval = settings.timerValue;
+    currentTimer = restTimeInterval;
+    circle.sweepAngle = 180;
+    display.poke();
+    setVariables()
+}
 
 function setPlaying() {
-
+    startButton.style.display = 'none'
 }
 
 clock.ontick = evt => {
-    loadSettings()
-    interval = settings.timerValue;
+    loadSettings();
     let today = evt.date;
     let hours = today.getHours();
     preferences.clockDisplay === "12h" ? hours = hours % 12 || 12 : hours = zeroPad(hours);
--- app/settingsUtil.js
+++ app/settingsUtil.js
@@ -1,19 +1,11 @@
-import {inbox} from "file-transfer";
-import {readFileSync} from "fs";
-import * as cbor from 'cbor';
-import document from 'document';
+import { inbox } from "file-transfer";
+import { readFileSync } from "fs";
 
 let defaultSettings = {
-    timerValue: 60,
+    restTimerValue: 60,
     backgroundColor: 'black'
 };
 inbox.onnewfile = processInbox;
-
-const time = document.getElementById("time-text");
-const arc = document.getElementById("circ");
-const bg = document.getElementById("bg");
-const timer = document.getElementById("countdown-text");
-const set = document.getElementById("set-text");
 
 export let settings = defaultSettings;
 
@@ -23,6 +15,8 @@ export const loadSettings = () => {
         transformSettings();
         mergeWithDefaultSettings();
     } catch (e) {
+        console.log('Settings Broken!')
+        console.log(e)
         settings = defaultSettings;
     }
 };
@@ -45,9 +39,18 @@ function processInbox() {
 }
 
 function transformSettings() {
-    if (settings.toggle) {
-        settings.timerValue = parseInt(settings.input.name);
+    if (settings.customRestToggle) {
+        settings.restTimerValue = parseInt(settings.customRestTime.name);
     } else {
-        settings.timerValue = settings.timerValue.values[0].value.valueOf();
+        settings.restTimerValue = settings.restTimerValue.values[0].value.valueOf();
+    }
+    try {
+        if (settings.customExerciseTimeToggle) {
+            settings.exerciseTimerValue = parseInt(settings.customExerciseTime.name);
+        } else {
+            settings.exerciseTimerValue = settings.exerciseTimerValue.values[0].value.valueOf();
+        }
+    } catch (e) {
+        settings.exerciseTimerValue = 30;
     }
 }
